46cd237a6d234f155465012b313bc360b9652152
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2020-11-19 20:07:14 +0000

    added player class.

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player
+{
+    private String name;
+    private int score;
+    private int count;
+
+    public Player(String name)
+    {
+        this.name = name;
+        score = 0;
+        count = 0;
+    }
+}
